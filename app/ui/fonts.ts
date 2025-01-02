1dcf94c8a678b3cdb23f161b67def4442f03731b
Eli Boninger <eboninger@gmail.com>
2025-01-02 13:50:20 -0500

    Add coloring and logo

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Nunito_Sans, Crimson_Pro } from "next/font/google";
+
+export const nunitoSans = Nunito_Sans({ subsets: ['latin'] })
+export const crimsonPro = Crimson_Pro({ subsets: ['latin'] })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        transparent: 'transparent',
+        main: {
+          '50': '#ecfcff',
+          '100': '#d4f6ff',
+          '200': '#b2f2ff',
+          '300': '#7decff',
+          '400': '#40ddff',
+          '500': '#14c2ff',
+          '600': '#00a3ff',
+          '700': '#008bff',
+          '800': '#007ae0',
+          '900': '#085ea0',
+          '950': '#0a3961',
+        },
       },
     },
   },
