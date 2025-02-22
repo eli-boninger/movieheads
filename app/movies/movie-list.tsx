5cf652cbc52c5cdf87338c3d32be062e3fd2fa49
Eli Boninger <eboninger@gmail.com>
2025-02-22 12:08:13 -0500

    use static params pattern for movie details

--- app/movies/movie-list.tsx
+++ app/movies/movie-list.tsx
@@ -2,10 +2,11 @@ import { List, ListItem } from "@mui/material"
 import MovieCard from "../ui/movie-card"
 import { use } from "react"
 import { ApiResponse } from "@/app/_lib/models/apiResponse"
+import Link from "next/link"
 
-export const MovieList = ({ movies }: { movies: Promise<{ data: ApiResponse }> }) => {
-    const { data } = use(movies)
+export const MovieList = ({ movies }: { movies: Promise<ApiResponse> }) => {
+    const data = use(movies)
     return (<List sx={{ display: 'flex', flexWrap: 'wrap', gap: '1rem', alignItems: 'center', alignContent: 'center', justifyContent: 'space-around' }}>
-        {data.results?.map(m => <ListItem key={m.id} sx={{ width: 'unset', p: 0, flexGrow: 2 }}><MovieCard movie={m} /></ListItem>)}
+        {data.results?.map(m => <ListItem key={m.id} sx={{ width: 'unset', p: 0, flexGrow: 2 }}><Link href={`/movies/${m.id}`}><MovieCard movie={m} /></Link></ListItem>)}
     </List>)
 }
--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,11 +1,21 @@
-import axios from "axios";
 import { Typography } from "@mui/material";
 import { Suspense } from "react";
 import { MovieList } from "./movie-list";
 import { ApiResponse } from "../_lib/models/apiResponse";
+import { notFound } from "next/navigation";
+
+
+async function getNowPlaying() {
+    const res = await fetch(`${process.env.TMDB_API_URL}/movie/now_playing?api_key=${process.env.TMDB_API_KEY}`, { cache: 'force-cache' })
+    const movies: ApiResponse = await res.json();
+    if (!movies) {
+        notFound();
+    }
+    return movies;
+}
 
 function Movies() {
-    const data = axios.get<ApiResponse>(`${process.env.TMDB_API_URL}/movie/now_playing?api_key=${process.env.TMDB_API_KEY}`)
+    const data = getNowPlaying();
     return (
         <section>
             <Typography variant="h1">NOW PLAYING</Typography>
